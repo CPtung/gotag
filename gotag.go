73993c2ac8e2b08c38251178c76a070fff9f82bc
justin <tungchengping@gmail.com>
2020-07-20 14:46:01 +0800

    fix: cannot create gotag.Tag directly

--- gotag.go
+++ gotag.go
@@ -1,77 +1,77 @@
 package gotag
 
 import (
-    "errors"
+	"errors"
 )
 
 type Tagf struct {
-    client  MsgQueueBase
+	client MsgQueueBase
 }
 
 func getHost(host []string) string {
-    if len(host) > 0 {
-        return host[0]
-    }
-    return ""
+	if len(host) > 0 {
+		return host[0]
+	}
+	return ""
 }
 
 func NewClient(host ...string) (*Tagf, error) {
-    config := &MQConfig{
-                Host: getHost(host),
-                Port: "1883",
-                Qos: 0,
-                Retained: false,
-                Debug: "error",
-             }
+	config := &MQConfig{
+		Host:     getHost(host),
+		Port:     "1883",
+		Qos:      0,
+		Retained: false,
+		Debug:    "error",
+	}
 
-    c, err := NewMqtt(config)
-    if err != nil {
-        return nil, err
-    }
+	c, err := newMqtt(config)
+	if err != nil {
+		return nil, err
+	}
 
-    return &Tagf{
-        client: c,
-    }, nil
+	return &Tagf{
+		client: c,
+	}, nil
 }
 
-func(self *Tagf) Publish(sourceName string, tagName string, val *Value, valType int32, timestamp uint64, unit string) error {
-    if !(self != nil && self.client != nil) {
-        return errors.New("tag client not found")
-    }
-    topic := EncodeTopic(sourceName, tagName)
-    payload := EncodePayload(sourceName, tagName, val, valType, timestamp, unit)
-    if payload == nil {
-        return errors.New("Invalid Input")
-    }
-    return self.client.Publish(topic, payload)
+func (self *Tagf) Publish(sourceName string, tagName string, val *Value, valType int32, timestamp uint64, unit string) error {
+	if !(self != nil && self.client != nil) {
+		return errors.New("tag client not found")
+	}
+	topic := EncodeTopic(sourceName, tagName)
+	payload := EncodePayload(sourceName, tagName, val, valType, timestamp, unit)
+	if payload == nil {
+		return errors.New("Invalid Input")
+	}
+	return self.client.Publish(topic, payload)
 }
 
-func(self *Tagf) Subscribe(sourceName, tagName string) error {
-    if !(self != nil && self.client != nil) {
-        return errors.New("tag client not found")
-    }
-    topic := EncodeTopic(sourceName, tagName)
-    return self.client.Subscribe(topic)
+func (self *Tagf) Subscribe(sourceName, tagName string) error {
+	if !(self != nil && self.client != nil) {
+		return errors.New("tag client not found")
+	}
+	topic := EncodeTopic(sourceName, tagName)
+	return self.client.Subscribe(topic)
 }
 
-func(self *Tagf) UnSubscribe(sourceName, tagName string) error {
-    if !(self != nil && self.client != nil) {
-        return errors.New("tag client not found")
-    }
-    topic := EncodeTopic(sourceName, tagName)
-    return self.client.UnSubscribe(topic)
+func (self *Tagf) UnSubscribe(sourceName, tagName string) error {
+	if !(self != nil && self.client != nil) {
+		return errors.New("tag client not found")
+	}
+	topic := EncodeTopic(sourceName, tagName)
+	return self.client.UnSubscribe(topic)
 }
 
-func(self *Tagf) SubscribeCallback(ontag OnTagCallback) error {
-    if !(self != nil && self.client != nil) {
-        return errors.New("tag client not found")
-    }
-    return self.client.SubscribeCallback(ontag)
+func (self *Tagf) SubscribeCallback(ontag OnTagCallback) error {
+	if !(self != nil && self.client != nil) {
+		return errors.New("tag client not found")
+	}
+	return self.client.SubscribeCallback(ontag)
 }
 
-func(self *Tagf) Delete() error {
-    if !(self != nil && self.client != nil) {
-        return errors.New("tag client not found")
-    }
-    return self.client.Close()
+func (self *Tagf) Delete() error {
+	if !(self != nil && self.client != nil) {
+		return errors.New("tag client not found")
+	}
+	return self.client.Close()
 }
